65248f856c2cb005d76a35464a9a67212f1288cb
Attila Nagy <nagyattila86.hu@gmail.com>
2023-03-10 13:58:20 +0100

    Create endpoints

--- server.py
+++ server.py
@@ -8,6 +8,21 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/voting-statistics')
+def voting():
+    return render_template('voting-statistics.html')
+
+
 if __name__ == "__main__":
     app.run(
         debug=True,
